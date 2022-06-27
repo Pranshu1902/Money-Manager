46ba4188f58f52df653584b3c07b7ad8d88f42b2
Pranshu1902 <pranshu1902@gmail.com>
2022-06-28 01:10:48 +0530

    token auth

--- money_manager/settings.py
+++ money_manager/settings.py
@@ -146,8 +146,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
